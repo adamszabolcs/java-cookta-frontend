77ee89b3fe31573d0127c94b9d29acf30fef217f
adamszabolcs <szabolcs.adam91@gmail.com>
2019-01-08 21:33:55 +0100

    added components to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import {FilterBar} from "./components/FilterBar";
+import {Navbar} from "./components/Navbar";
+import {SearchBar} from "./components/SearchBar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <Navbar/>
+          <SearchBar/>
           <FilterBar/>
       </div>
     );
