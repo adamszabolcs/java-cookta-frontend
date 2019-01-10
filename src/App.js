cf6ff3c5f7115f25b8af3bf214e33941b883395d
Gábor Kovács <gabor950601@gmail.com>
2019-01-10 18:04:31 +0100

    search trying, commit before pull

--- src/App.js
+++ src/App.js
@@ -14,11 +14,14 @@ class App extends Component {
         super(props);
         let basurl = "https://api.edamam.com/search?q=chicken&app_id=5b5897f7&app_key=9ac6d44f07118d8a2bead5a790b270d5&from=0&to=10&calories=591-722&health=alcohol-free"
 
+        this.handleSubmit = this.handleSubmit.bind(this);
+
         this.state = {
             hits: [],
             isLoading: false,
             homeUrl: "http://192.168.160.73:8080/",
-            searchUrl: "http://192.168.160.73:8080/search/q="
+            searchUrl: "http://192.168.160.73:8080/search/q=",
+            searchprase: ""
         };
     }
 
@@ -26,29 +29,51 @@ class App extends Component {
     componentDidMount() {
         this.setState({isLoading: true});
 
-        fetch(this.state.homeUrl)
+        //this.performSearch();
+
+        fetch("http://192.168.160.73:8080/api")
             .then(response => response.json())
             .then(data => this.setState({hits: data, isLoading: false}));
     }
 
+    handleSubmit(event) {
+        event.preventDefault();
 
-    handleSubmit = () => {
-        let url = window.location.href;
-        //url = url.split("/")[3];
-        alert(url);
+        console.log(event.target);
+        const data = new FormData(event.target);
+        console.log(data);
+        fetch("http://192.168.160.73:8080/api/search", {
+            method: 'POST',
+            body: data,
+        });
+    }
 
-        /*this.setState({homeUrl: "http://192.168.160.73:8080/search/q=" + url.split("/")[2]})
-        console.log(this.state.homeUrl);*/
-
-        /*fetch("http://192.168.160.73:8080/", {
-            method: 'GET',
-            body: url,
-        })
-            .then(response => response.json())
-            .then(data => this.setState({hits: data, isLoading: false}));*/
+    handleChange(event) {
+        this.setState({searchprase: event.target.value});
     }
 
 
+   /* performSearch = (query = false) => {
+        let passed = "";
+        if(query){
+            let searchy = this.handleSubmit();
+            passed = "search/" + searchy;
+        }
+        fetch("http://192.168.160.73:8080/api/"+passed)
+            .then(response => response.json())
+            .then(responseData => {this.setState({hits: responseData, isLoading: false
+            });
+            })
+            .catch(error => {
+                console.log('Error fetching and parsing data', error);
+            });
+    }*/
+
+    /*handleSubmit() {
+        let url = window.location.href;
+        alert(url);
+        return url.split("/")[3];
+    }*/
 
     render() {
         const { hits, isLoading } = this.state;
@@ -57,7 +82,9 @@ class App extends Component {
             <div className="App">
                 <Navbar/>
                 <SearchBar
+                    searchprase={this.state.searchprase}
                     onSubmit={this.handleSubmit}
+                    onChange={this.handleChange}
                 />
                 <Featured/>
                 <Recipes
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -18,19 +18,19 @@ export class SearchBar extends Component {
                     </div>
                     <div className="row tm-banner-row" id="tm-section-search">
 
-                        <form method="get" className="tm-search-form tm-section-pad-2">
+                        <form onSubmit={this.props.onSubmit} method="POST" className="tm-search-form tm-section-pad-2">
                             <div className="form-row tm-search-form-row">
                                 <div className="form-group tm-form-group tm-form-group-pad tm-form-group-1">
                                     <label htmlFor="inputCity">Search for recipe</label>
                                     <input name="q" type="text" className="form-control" id="inputCity"
-                                           placeholder="Search for recipe..."/>
+                                           placeholder="Search for recipe..." value={this.props.searchprase} onChange={this.props.onChange}/>
                                 </div>
                             </div>
                             <FilterBar/>
                             <div className="form-group tm-form-group tm-form-group-pad tm-form-group-1">
                                 <label htmlFor="btnSubmit">&nbsp;</label>
                                 <button type="submit" className="btn btn-primary tm-btn tm-btn-search text-uppercase"
-                                        onClick={this.props.onSubmit}
+                                        /*onClick={() => this.props.onSubmit(true)}*/
                                         id="btnSubmit">Submit search
                                 </button>
                             </div>
